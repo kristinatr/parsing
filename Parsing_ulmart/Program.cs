17f620f84a6d8af8d6dec3ad0e91a44aa7dc82b2
kristinatr <kristinatroilova@gmail.com>
2019-12-23 02:04:16 +0300

    add consol app of parsing site with anglesharp

---
+++ Parsing_ulmart/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+
+namespace Parsing_ulmart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+
+            string source = "https://www.ulmart.ru/goods/4534822";
+
+            var document = await context.OpenAsync(source);
+
+            var breadcrumbs = document.QuerySelectorAll("nav").Where(x => x.ClassName.Contains("b-crumbs b-crumbs_theme_normal")).First();
+            List<string> breadcrumbsTitle = breadcrumbs.QuerySelectorAll("span[itemprop='title']").Select(x => x.Text()).ToList();
+            string name = document.QuerySelectorAll("h1").Where(x => x.ClassName.Contains("main-h1 main-h1_bold js-reload")).First().Text().Trim('\n', ' ');
+            string num = document.QuerySelector("span.b-art__num").Text();
+            string oldPrice = document.QuerySelector("div.b-product-card__price").QuerySelectorAll("span").First().Text();
+            string newPrice = document.QuerySelector("span.b-price__num").Text();
+            string currency = document.QuerySelector("span.b-price__sign").Text();
+            string availability = document.QuerySelector("div#p-stat").QuerySelector("div").Text().Trim('\n', ' ');
+            string imageUrl = document.QuerySelector("div#product_card_img").QuerySelector("img").GetAttribute("src");
+
+            Console.Write("Категории: ");
+            foreach (var item in breadcrumbsTitle)
+            {
+                if (item == breadcrumbsTitle.Last()) {
+                    Console.Write(item);
+                    break;
+                }
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("\nНазвание: " + name);
+            Console.WriteLine("Артикул: " + num);
+            Console.WriteLine(String.Format("Старая цена: {0} {1}", oldPrice, currency));
+            Console.WriteLine(String.Format("Новая цена: {0} {1}", newPrice, currency));
+            Console.WriteLine("Наличие: " + availability);
+            Console.WriteLine("Ссылка на картинку: " + imageUrl);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parsing_ulmart/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Parsing_ulmart")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Parsing_ulmart")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c94a4c7e-ff89-4e26-9b09-7bc51f03f990")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
